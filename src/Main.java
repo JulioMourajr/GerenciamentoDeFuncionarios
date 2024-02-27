eb418b16981c41ac25626f3824d25f2731bdeb67
Julio Moura <juliomourajr@hotmail.com>
2024-02-27 03:34:32 -0300

    Criando Metodos na Classe Main.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+public class Main {
+    static ArrayList<Funcionario> funcionario = new ArrayList<>();
+
+    public static void addFuncionario(Scanner leitor) {
+        System.out.println("Insira o nome do funcionário: ");
+        String nome = leitor.nextLine();
+        System.out.println("Insira o cpf do funcionário: ");
+        String cpf = leitor.nextLine();
+        System.out.println("Insira o salaário do funcionário: ");
+        double salario = Double.parseDouble(leitor.nextLine());
+        System.out.println("Insira a matricula do funcionário: ");
+        String matricula = leitor.nextLine();
+        System.out.println("Insira o departamento do funcionário: ");
+        String departamento = leitor.nextLine();
+
+        Funcionario novoFuncionario = new Funcionario(nome, cpf, salario, matricula, departamento);
+        funcionario.add(novoFuncionario);
+    }
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Bem vindo ao sistema de gerenciamento de funcionários!");
+        addFuncionario(leitor);
+        funcionario.forEach(System.out::println);
+
     }
 }

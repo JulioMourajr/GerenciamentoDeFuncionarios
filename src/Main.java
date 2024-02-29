8628e520c1392fe38701ef82f15b1f1741144954
Julio Moura <juliomourajr@hotmail.com>
2024-02-29 05:30:51 -0300

    Modificações no metodo Main

--- src/Main.java
+++ src/Main.java
@@ -20,11 +20,37 @@ public class Main {
         Funcionario novoFuncionario = new Funcionario(nome, cpf, salario, matricula, departamento);
         funcionario.add(novoFuncionario);
     }
+
+    public static void removeFuncionario(Scanner func) {
+        funcionario.remove(func);
+    }
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        String texto;
+        String opcao;
         System.out.println("Bem vindo ao sistema de gerenciamento de funcionários!");
-        addFuncionario(leitor);
-        funcionario.forEach(System.out::println);
-
+        do{
+            texto = "Digite 1 para adicionar um novo funcionário \n " +
+                    "Digite 2 para remover um funcionário \n ou 3 para consultar" +
+                    " todos os funcionários ou (digite n para parar" ;
+            System.out.println(texto);
+            opcao = leitor.nextLine();
+            if(leitor.nextLine().equals("n")){
+                break;
+            }
+        }
+        while(!opcao.equals("1") && !opcao.equals("2") && !opcao.equals("3"));
+        if (opcao.equals("1")) {
+            System.out.println("Adicionando um novo funcionário");
+            addFuncionario(leitor);
+        } else if (opcao.equals("2")) {
+            System.out.println("Removendo um funcionário");
+            removeFuncionario(leitor);
+        } else if (opcao.equals("3")) {
+            for (Funcionario func : funcionario) {
+                System.out.println("Funcionário: ");
+                System.out.println(func);
+            }
+        }
     }
 }
